edd03973370144310a674ed87bdf47580558e9cc
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-17 13:45:41 +0000

    test: #9 write the tests for the main activity presenter

--- app/src/test/java/io/github/alexcosta97/unify/MainActivityPresenterTest.java
+++ app/src/test/java/io/github/alexcosta97/unify/MainActivityPresenterTest.java
@@ -1,4 +1,48 @@
 package io.github.alexcosta97.unify;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import io.github.alexcosta97.unify.Presenters.MainActivityPresenter;
+import io.github.alexcosta97.unify.Views.MainActivityView;
+
+@RunWith(MockitoJUnitRunner.class)
 public class MainActivityPresenterTest {
+    MainActivityPresenter presenter;
+
+    @Mock
+    MainActivityView view;
+
+    @Before
+    public void setUp() throws Exception{
+        presenter = new MainActivityPresenter(view);
+    }
+
+    @Test
+    public void signInButtonPressed() throws Exception{
+        //Arrange
+        Class expectedClass = SignInActivity.class;
+
+        //Act
+        presenter.signInButtonPressed();
+
+        //Assert
+        Mockito.verify(view).launchNextActivity(expectedClass);
+    }
+
+    @Test
+    public void signUpButtonPressed() throws Exception{
+        //Arrange
+        Class expectedClass = AddCompanyActivity.class;
+
+        //Act
+        presenter.signUpButtonPressed();
+
+        //Assert
+        Mockito.verify(view).launchNextActivity(expectedClass);
+    }
 }
