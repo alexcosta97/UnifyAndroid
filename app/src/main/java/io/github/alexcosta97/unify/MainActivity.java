c28c6ede478274d000b8cf138c04dbbae608b092
Alexandre Costa <alex.callum.c@gmail.com>
2019-01-17 14:24:06 +0000

    feat: #9 write the code to pass MainActivity Test and implement Material theme

--- app/src/main/java/io/github/alexcosta97/unify/MainActivity.java
+++ app/src/main/java/io/github/alexcosta97/unify/MainActivity.java
@@ -1,13 +1,45 @@
 package io.github.alexcosta97.unify;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+import io.github.alexcosta97.unify.Presenters.MainActivityPresenter;
+import io.github.alexcosta97.unify.Views.MainActivityView;
+
+public class MainActivity extends AppCompatActivity implements MainActivityView {
+    Button signInButton;
+    Button signUpButton;
+    MainActivityPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        presenter = new MainActivityPresenter(this);
+
+        signInButton = findViewById(R.id.signInButton);
+        signUpButton = findViewById(R.id.signUpButton);
+
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.signInButtonPressed();
+            }
+        });
+
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                presenter.signUpButtonPressed();
+            }
+        });
+    }
+
+    public void launchNextActivity(Class newActivity){
+        Intent intent = new Intent(MainActivity.this, newActivity);
+        startActivity(intent);
     }
 }
